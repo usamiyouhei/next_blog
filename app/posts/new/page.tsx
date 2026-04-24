0e4321407be1b3ade4abc955533e104f533a2eda
youhei usami <usao9110@icloud.com>
2026-04-24 12:12:00 +0400

    4/24

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -1,11 +1,12 @@
 import PostForm from "@/components/PostForm";
 import "./page.css";
+import { createPost } from "@/lib/actions";
 
 export default function NewPostPage() {
   return (
     <div className="page">
       <h1 className="page-title">新規記事作成</h1>
-      <PostForm />
+      <PostForm action={createPost} />
     </div>
   );
 }
