0666e40f1caabaa38dc71c27622e2dbe1d2642c8
youhei usami <usao9110@icloud.com>
2026-04-28 08:57:25 +0400

    4/28

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,14 +1,17 @@
 import { Post } from "@/entities/post";
 import { getDataSource } from "./db";
 
-export async function getPosts() {
+export async function getPosts(options: { page?: number; limit?: number }) {
+  const { page = 1, limit = 2 } = options;
   const ds = await getDataSource();
   const postRepo = ds.getRepository(Post);
 
   const [posts, total] = await postRepo.findAndCount({
     relations: ["user"],
     order: { createdAt: "DESC" },
+    skip: (page - 1) * limit,
+    take: limit,
   });
 
-  return { posts, total };
+  return { posts, total, page, totalPage: Math.ceil(total / limit) };
 }
